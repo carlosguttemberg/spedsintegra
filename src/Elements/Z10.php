8ce7fce8209bfc3298e8cfac61dfbdbc3384d414
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-06-23 08:13:26 -0300

    Alterando o registro 10

--- src/Elements/Z10.php
+++ src/Elements/Z10.php
@@ -3,25 +3,29 @@
 namespace NFePHP\Sintegra\Elements;
 
 use NFePHP\Sintegra\Common\Element;
+use NFePHP\Sintegra\Common\ElementInterface;
+use \stdClass;
 
-class Z10 extends Element
+class Z10 extends Element implements ElementInterface
 {
     const REGISTRO = '10';
+    const LEVEL = 0;
+    const PARENT = '';
 
     protected $parameters = [
         'CNPJ' => [
-            'type' => 'numeric',
-            'regex' => '^[0-9]{11,14}$',
-            'required' => true,
+            'type' => 'string',
+            'regex' => '^[0-9]{14}$',
+            'required' => false,
             'info' => 'Número de inscrição do estabelecimento matriz da pessoa jurídica no CNPJ.',
-            'format' => 'totalNumber',
+            'format' => '',
             'length' => 14
         ],
         'IE' => [
             'type' => 'string',
-            'regex' => '^ISENTO|[0-9]{0,14}$',
+            'regex' => '^[0-9]{2,14}$',
             'required' => false,
-            'info' => 'Número de inscrição do estadual.',
+            'info' => 'Número de inscrição do estudal.',
             'format' => '',
             'length' => 14
         ],
@@ -37,13 +41,13 @@ class Z10 extends Element
             'type' => 'string',
             'regex' => '^.{2,30}$',
             'required' => true,
-            'info' => 'Municipio do estabelecimento',
+            'info' => 'Municipio do estabelicimento',
             'format' => '',
             'length' => 30
         ],
         'UF' => [
             'type' => 'string',
-            'regex' => '^(AC|AL|AM|AP|BA|CE|DF|ES|GO|MA|MG|MS|MT|PA|PB|PE|PI|PR|RJ|RN|RO|RR|RS|SC|SE|SP|TO)$',
+            'regex' => '^.{2}$',
             'required' => true,
             'info' => 'Sigla da Unidade da Federação da pessoa',
             'format' => '',
@@ -51,15 +55,15 @@ class Z10 extends Element
         ],
         'FAX' => [
             'type' => 'string',
-            'regex' => '^[0-9]{5,10}$',
+            'regex' => '^.{10}$',
             'required' => false,
             'info' => 'Telefone do estabelicimento',
-            'format' => 'totalNumber',
+            'format' => '',
             'length' => 10
         ],
         'DT_INI' => [
             'type' => 'string',
-            'regex' => '^(2[0-9]{3})(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01])$',
+            'regex' => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
             'required' => true,
             'info' => 'Data inicial das informações contidas no arquivo.',
             'format' => '',
@@ -67,7 +71,7 @@ class Z10 extends Element
         ],
         'DT_FIM' => [
             'type' => 'string',
-            'regex' => '^(2[0-9]{3})(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01])$',
+            'regex' => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
             'required' => true,
             'info' => 'Data final das informações contidas no arquivo.',
             'format' => '',
@@ -75,24 +79,24 @@ class Z10 extends Element
         ],
         'COGIGO_MAGNETICO' => [
             'type' => 'numeric',
-            'regex' => '^(1|2|3)$',
-            'required' => true,
+            'regex' => '^(\d{1})$',
+            'required' => false,
             'info' => 'Código da identificação da estrutura do arquivo magnético entregue',
             'format' => '',
             'length' => 1
         ],
         'COGIGO_NATUREZAS' => [
             'type' => 'numeric',
-            'regex' => '^(1|2|3)$',
-            'required' => true,
+            'regex' => '^(\d{1})$',
+            'required' => false,
             'info' => 'Código da identificação da natureza das operações informadas',
             'format' => '',
             'length' => 1
         ],
         'COGIGO_FINALIDADE' => [
             'type' => 'numeric',
-            'regex' => '^(1|2|3|5)$',
-            'required' => true,
+            'regex' => '^(\d{1})$',
+            'required' => false,
             'info' => 'Código da finalidade do arquivo magnético',
             'format' => '',
             'length' => 1
@@ -107,25 +111,5 @@ class Z10 extends Element
     {
         parent::__construct(self::REGISTRO);
         $this->std = $this->standarize($std);
-        $this->postValidation();
-    }
-
-    /**
-     * Validação secundária sobre as data informadas
-     * @throws \Exception
-     */
-    public function postValidation()
-    {
-        $dini = \DateTime::createFromFormat('Ymd', $this->std->dt_ini);
-        $dfim = \DateTime::createFromFormat('Ymd', $this->std->dt_fim);
-        $lastday = date("Ymt", strtotime($dini->format('Y-m-d')));
-        if ($dfim <= $dini) {
-            throw new \Exception("Erro: Bloco1, campo 10 - A data final deve "
-                . "ser maior que a data inicial.");
-        }
-        if ($this->std->dt_fim != $lastday) {
-            throw new \Exception("Erro: Bloco1, campo 10 - A data final deve "
-                . "ser o último dia do mês indicado na data inicial.");
-        }
     }
 }
